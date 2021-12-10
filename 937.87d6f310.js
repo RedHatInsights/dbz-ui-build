110a6ea9a6e2f92b52bca53dacfdae0249295813
Red Hat Application Services CI <app-services-ci@users.noreply.github.com>
2021-12-10 10:01:02 +0000

    Update to b867d54

---
+++ 937.87d6f310.js
@@ -0,0 +1,3 @@
+/*! For license information please see 937.87d6f310.js.LICENSE.txt */
+"use strict";(self.webpackChunk_debezium_ui=self.webpackChunk_debezium_ui||[]).push([[937],{85937:(e,n,i)=>{i.r(n),i.d(n,{ContentTypes:()=>u});var u=function(){function e(){}return e.APPLICATION_JSON="application/json",e.APPLICATION_XML="application/xml",e}()}}]);
+//# sourceMappingURL=937.87d6f310.js.map
